2e3f6f88ee7901501fe3d2c7b65cfe0427b0fcd8
verysick <verysick6666@yandex.ru>
2018-12-11 17:09:51 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+# -*- coding: utf-8 -*-
+import sys
+from time import sleep
+
+print('Скрипт запустился')
+sys.stdout.write('hello, world')
+sys.stdout.flush()
+sleep(2)
+sys.stdout.write('privet mir')
