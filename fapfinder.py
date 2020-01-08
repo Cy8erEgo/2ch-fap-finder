e7fdd169dc371f798d01ef4f6b27e21e7d3c49cb
Cyberego <cy8erego@yandex.ru>
2020-01-09 00:14:22 +0700

    Thread headers will now be parsed

--- fapfinder.py
+++ fapfinder.py
@@ -2,6 +2,7 @@
 
 from models import TwoChannel
 from prettytable import PrettyTable
+from bs4 import BeautifulSoup as BS
 
 
 def main():
@@ -15,6 +16,7 @@ def main():
 
     for i, t in enumerate(fap_threads, 1):
         subject = t["subject"] if len(t["subject"]) <= 40 else t["subject"][:37] + "..."
+        subject = BS(subject, "html.parser").get_text()
         score = round(float(t["score"]), 2)
         webm_count, imgs_count = ch.get_media_count(t["num"])
         url = "https://2ch.hk/b/res/%s.html" % t["num"]
